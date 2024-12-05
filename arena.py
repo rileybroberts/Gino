f98cb0de83f1cef4dc355388c40ad66f242d1e78
syanification <syanification@gmail.com>
2024-12-05 12:29:30 -0500

    added demo function

--- arena.py
+++ arena.py
@@ -82,10 +82,49 @@ class Arena:
         ginoRewards, ginoWinrate = self.ginoActor(numEpisodes)
         randoRewards, randoWinrate = self.randomActor(numEpisodes)
 
+        print(f"\nResults:")
         print(f"Gino --------- Total Rewards: {ginoRewards}")
         print(f"Gino -------------- Win Rate: {ginoWinrate}")
         print(f"Random Actor - Total Rewards: {randoRewards}")
         print(f"Random Actor ------ Win Rate: {randoWinrate}")
 
+    def demo(self):
+        userInput = "p"
+
+        while userInput != "stop":
+
+            print("Dealing fresh hand...")
+
+            stock = deck.Deck()
+            stock.shuffleDeck()
+
+            hand = []
+            for x in range(11):
+                hand.append(stock.dealCard())
+            hand.sort()
+
+            reward = self.gino.reward(hand)
+
+            while reward!= 10 and stock.cardsLeft() > 1 and userInput != "stop":
+                reward = self.gino.reward(hand)
+
+                print(f"\nCurrent Hand: {hand}")
+
+                if (reward == 10):
+                    print("Gin! Gino Wins!")
+                    userInput = input("Press Enter to Continue, Type 'stop' to Stop: ")
+                    break
+
+                
+                userInput = input("Press Enter to Continue, Type 'stop' to Stop: ")
+
+                del hand[self.gino.policy(hand)]
+                # print(stock.cardStack)
+                hand.append(stock.dealCard())
+                hand.sort()
+
+
 arena = Arena()
+arena.demo()
+print("Battling Gino Against A Random Actor 10k Times...")
 arena.battle(100000)
